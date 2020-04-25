87d59efef27d217b6470a1f7f09a3cca11c15831
parkjunghun <go9018@naver.com>
2020-04-25 15:22:13 +0900

    navigation test samples

--- App.js
+++ App.js
@@ -6,17 +6,23 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from './view/HomeScreen';
+import SearchScreen from './view/SearchScreen';
+
+const Tab = createBottomTabNavigator();
 
 function App() {
-  return(
-      <View>
-        <Text>JungHoon's TestApp</Text>
-      </View>
-  )
+    return (
+        <NavigationContainer>
+            <Tab>
+                <Tab.Screen name = "Home" component = {HomeScreen}/>
+                <Tab.Screen name = "Search" component = {SearchScreen}/>
+            </Tab>
+        </NavigationContainer>
+    )
 }
+
 export default App;
